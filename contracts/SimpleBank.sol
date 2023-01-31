59ffed71c608cb90277ea6a348c06e73cc21957e
johann BICH <johann.bich@helvix.de>
2023-01-31 09:38:58 +0100

    WIP simpleBank with fiat tokens

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+interface IMyToken is IERC20 {
+  function mint(address to, uint256 amount) external;
+}
 
 contract SimpleBank {
   // bank owner
@@ -11,8 +15,15 @@ contract SimpleBank {
   // available currencies
   mapping(string => bool) public checkCurrencies;
 
+  // Currency struct
+  struct Currency {
+    string code;
+    IMyToken location;
+    AggregatorV3Interface priceFeed;
+  }
+
   // list of currencies
-  string[] public currencies = ["EUR", "USD", "GBP"];
+  Currency[] public currencies;
 
   // registered clients
   mapping(address => bool) public registered;
@@ -20,16 +31,30 @@ contract SimpleBank {
   // balances for each currencies
   mapping(address => mapping(string => uint)) internal balances;
 
-  constructor() {
+  constructor(IMyToken _eurContract, IMyToken _usdContract, IMyToken _gbpContract, AggregatorV3Interface _eurPrice, AggregatorV3Interface _usdPrice, AggregatorV3Interface _gbpPrice) {
     owner = msg.sender;
     // make the owner the first registered client of our bank
     registered[owner] = true;
     // initialize default currencies
-    // extract length to avoid computation at each loop
-    uint currenciesLength = currencies.length;
-    for (uint i = 0; i < currenciesLength; i++) {
-      checkCurrencies[currencies[i]] = true;
-    }
+    currencies.push(Currency({
+      code: "EUR",
+      location: _eurContract,
+      priceFeed: AggregatorV3Interface(_eurPrice)
+    }));
+    currencies.push(Currency({
+      code: "USD",
+      location: _usdContract,
+      priceFeed: AggregatorV3Interface(_usdPrice)
+    }));
+    currencies.push(Currency({
+      code: "GBP",
+      location: _gbpContract,
+      priceFeed: AggregatorV3Interface(_gbpPrice)
+    }));
+    // initialize checkCurrencies mapping
+    checkCurrencies["EUR"] = true;
+    checkCurrencies["USD"] = true;
+    checkCurrencies["GBP"] = true;
   }
   
   /**
@@ -58,6 +83,7 @@ contract SimpleBank {
   event LogRegister(address accountAddress);
   event LogDeposit(address accountAddress, string currency, uint amount);
   event LogWithdraw(address accountAddress, string currency, uint amount);
+  event LogBuyToken(address accountAddress, string currency, uint amountGiven, uint amountReceive);
 
   /**
   * FUNCTIONS
@@ -95,11 +121,32 @@ contract SimpleBank {
       return true;
     }
 
-  function addNewCurrency(string memory code)
+  function addNewCurrency(string memory _code, IMyToken _newCurrency, AggregatorV3Interface _priceFeed)
     onlyOwner
     public returns (bool) {
-      currencies.push(code);
-      checkCurrencies[code] = true;
+      currencies.push(Currency({
+        code: _code,
+        location: _newCurrency,
+        priceFeed: _priceFeed
+      }));
+      checkCurrencies[_code] = true;
       return true;
     }
+
+  // Function to buy tokens (EUR, USD, ...) with ETH
+  // Uses chainlink oracle to get price of corresponding pair
+  function buyTokens(string memory _currency)
+    onlyExistingCurrencies(_currency)
+    public payable 
+    returns (uint amountToBeGiven) {
+      uint256 paymentReceived = msg.value;
+      for (uint256 i = 0; i < currencies.length; i++) {
+        if (keccak256(bytes(currencies[i].code)) == keccak256(bytes(_currency))) {
+          (,int price,,,) = currencies[i].priceFeed.latestRoundData();
+          amountToBeGiven = paymentReceived * uint(price) / 1 ether;
+          currencies[i].location.mint(msg.sender, amountToBeGiven);
+          emit LogBuyToken(msg.sender, _currency, msg.value, amountToBeGiven);
+        }
+      }
+  }
 }
